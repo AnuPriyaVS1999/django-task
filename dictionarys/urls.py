050c9c95b4621f884d79ddf1b362d2e73b540661
AnuPriyaVS1999 <AnupriyaVS1114@gmail.com>
2023-06-07 02:30:32 -0700

    html page styled

--- dictionarys/urls.py
+++ dictionarys/urls.py
@@ -1,10 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import setting
+from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path('', include("web.urls", namespace="web")),
+    path('', include("web.urls", namespace="web")),
 ]+ static (settings.MEDIA_URL, documents_root = settings.MEDIA_ROOT)
