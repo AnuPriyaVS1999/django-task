9888b3b575a0eced75a07e61a8027a03104e32e0
AnuPriyaVS1999 <AnupriyaVS1114@gmail.com>
2023-06-06 23:13:33 -0700

    html page structured

--- dictionarys/settings.py
+++ dictionarys/settings.py
@@ -125,3 +125,9 @@ STATICFILES_DIRS =[
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+MEADIA_URL ='/media/'
+MEDIA_ROOT = BASE_DIR /"media/"
+
